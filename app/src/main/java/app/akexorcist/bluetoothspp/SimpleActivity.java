e6fa1ab8162622ff867e6f2b6c58aa3f460d4aba
skyforce <skyforce@gmail.com>
2019-04-13 01:14:08 +0800

    add garminhud

--- app/src/main/java/app/akexorcist/bluetoothspp/SimpleActivity.java
+++ app/src/main/java/app/akexorcist/bluetoothspp/SimpleActivity.java
@@ -16,114 +16,123 @@
 
 package app.akexorcist.bluetoothspp;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
+
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
-import app.akexorcist.bluetotohspp.library.BluetoothState;
 import app.akexorcist.bluetotohspp.library.BluetoothSPP.BluetoothConnectionListener;
 import app.akexorcist.bluetotohspp.library.BluetoothSPP.OnDataReceivedListener;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
 import app.akexorcist.bluetotohspp.library.DeviceList;
+import sky4s.garminhud.GarminHUD;
+import sky4s.garminhud.eOutAngle;
 
 public class SimpleActivity extends Activity {
-	BluetoothSPP bt;
-	
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_simple);
-		
-		bt = new BluetoothSPP(this);
+    BluetoothSPP bt;
 
-		if(!bt.isBluetoothAvailable()) {
-			Toast.makeText(getApplicationContext()
-					, "Bluetooth is not available"
-					, Toast.LENGTH_SHORT).show();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple);
+
+        bt = new BluetoothSPP(this);
+
+        if (!bt.isBluetoothAvailable()) {
+            Toast.makeText(getApplicationContext()
+                    , "Bluetooth is not available"
+                    , Toast.LENGTH_SHORT).show();
             finish();
-		}
-		
-		bt.setOnDataReceivedListener(new OnDataReceivedListener() {
-			public void onDataReceived(byte[] data, String message) {
-				Toast.makeText(SimpleActivity.this, message, Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		bt.setBluetoothConnectionListener(new BluetoothConnectionListener() {
-			public void onDeviceConnected(String name, String address) {
-				Toast.makeText(getApplicationContext()
-						, "Connected to " + name + "\n" + address
-						, Toast.LENGTH_SHORT).show();
-			}
+        }
 
-			public void onDeviceDisconnected() {
-				Toast.makeText(getApplicationContext()
-						, "Connection lost", Toast.LENGTH_SHORT).show();
-			}
+        bt.setOnDataReceivedListener(new OnDataReceivedListener() {
+            public void onDataReceived(byte[] data, String message) {
+                Toast.makeText(SimpleActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
 
-			public void onDeviceConnectionFailed() {
-				Toast.makeText(getApplicationContext()
-						, "Unable to connect", Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		Button btnConnect = (Button)findViewById(R.id.btnConnect);
-		btnConnect.setOnClickListener(new OnClickListener(){
-        	public void onClick(View v){
-        		if(bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
-        			bt.disconnect();
-        		} else {
+        bt.setBluetoothConnectionListener(new BluetoothConnectionListener() {
+            public void onDeviceConnected(String name, String address) {
+                Toast.makeText(getApplicationContext()
+                        , "Connected to " + name + "\n" + address
+                        , Toast.LENGTH_SHORT).show();
+            }
+
+            public void onDeviceDisconnected() {
+                Toast.makeText(getApplicationContext()
+                        , "Connection lost", Toast.LENGTH_SHORT).show();
+            }
+
+            public void onDeviceConnectionFailed() {
+                Toast.makeText(getApplicationContext()
+                        , "Unable to connect", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnConnect = (Button) findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
+                    bt.disconnect();
+                } else {
                     Intent intent = new Intent(getApplicationContext(), DeviceList.class);
-                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE); 
-        		}
-        	}
-        }); 
-	}
-	
-	public void onDestroy() {
+                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+                }
+            }
+        });
+    }
+
+    public void onDestroy() {
         super.onDestroy();
         bt.stopService();
     }
-	
-	public void onStart() {
+
+    public void onStart() {
         super.onStart();
         if (!bt.isBluetoothEnabled()) {
-        	Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
         } else {
-            if(!bt.isServiceAvailable()) { 
+            if (!bt.isServiceAvailable()) {
                 bt.setupService();
-                bt.startService(BluetoothState.DEVICE_ANDROID);
+                bt.startService(BluetoothState.DEVICE_OTHER);
                 setup();
             }
         }
     }
-	
-	public void setup() {
-		Button btnSend = (Button)findViewById(R.id.btnSend);
-		btnSend.setOnClickListener(new OnClickListener(){
-        	public void onClick(View v){
-        		bt.send("Text", true);
-        	}
+
+    public void setup() {
+        Button btnSend = (Button) findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+//        		bt.send("Text", true);
+                GarminHUD hud = new GarminHUD(bt);
+                hud.SetDirection(eOutAngle.EasyRight);
+                String message = hud.getSendResult() ? "Send to Garmin HUD success" : "Send to Garmin HUD failed";
+                Toast.makeText(getApplicationContext()
+                        , message, Toast.LENGTH_SHORT).show();
+            }
         });
-	}
-	
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
-			if(resultCode == Activity.RESULT_OK)
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK)
                 bt.connect(data);
-		} else if(requestCode == BluetoothState.REQUEST_ENABLE_BT) {
-            if(resultCode == Activity.RESULT_OK) {
+        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+//                bt.setDeviceTarget(BluetoothState.DEVICE_OTHER);
                 bt.setupService();
-                bt.startService(BluetoothState.DEVICE_ANDROID);
+                bt.startService(BluetoothState.DEVICE_OTHER);
                 setup();
             } else {
                 Toast.makeText(getApplicationContext()
-                		, "Bluetooth was not enabled."
-                		, Toast.LENGTH_SHORT).show();
+                        , "Bluetooth was not enabled."
+                        , Toast.LENGTH_SHORT).show();
                 finish();
             }
         }
